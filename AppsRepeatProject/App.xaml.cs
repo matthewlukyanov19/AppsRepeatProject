9018768886a1fb06196dc027c1c88074eae81b25
matthewlukyanov19 <brucedog9@gmail.com>
2024-08-18 16:32:18 +0100

    Trying to fix Home page, running into errors with 'initializecomponent'

--- AppsRepeatProject/App.xaml.cs
+++ AppsRepeatProject/App.xaml.cs
@@ -7,7 +7,7 @@ namespace AppsRepeatProject
         public App()
         {
             InitializeComponent();
-            MainPage = new NavigationPage(new HomePage());
+            MainPage = new NavigationPage(new Home());
         }
 
         
--- AppsRepeatProject/Home.xaml.cs
+++ AppsRepeatProject/Home.xaml.cs
@@ -2,22 +2,20 @@ using Microsoft.Maui.Controls;
 
 namespace AppsRepeatProject
 {
-    public partial class HomePage : ContentPage
+    public partial class Home : ContentPage
     {
-        public HomePage()
+        public Home()
         {
             InitializeComponent();
         }
 
         private async void OnStartGameClicked(object sender, EventArgs e)
         {
-            // Navigate to the MainPage
             await Navigation.PushAsync(new MainPage());
         }
 
         private async void OnGoToSettingsClicked(object sender, EventArgs e)
         {
-            // Navigate to the SettingsPage
             await Navigation.PushAsync(new SettingsPage());
         }
     }
