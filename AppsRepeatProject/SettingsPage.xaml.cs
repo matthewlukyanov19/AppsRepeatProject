137ca0481c9bc1f0173b3fee278a6096b502e9f3
matthewlukyanov19 <brucedog9@gmail.com>
2024-08-22 18:33:39 +0100

    Updated the Settings page to include light/dark mode plus timer adjustment.

--- AppsRepeatProject/SettingsPage.xaml.cs
+++ AppsRepeatProject/SettingsPage.xaml.cs
@@ -12,7 +12,27 @@ namespace AppsRepeatProject
             // Navigate back to the previous page
             await Navigation.PopAsync();
         }
+
+        private void OnDarkModeToggled(object sender, ToggledEventArgs e)
+        {
+            // Toggle between dark and light mode
+            if (e.Value)
+            {
+                Application.Current.UserAppTheme = AppTheme.Dark;
+            }
+            else
+            {
+                Application.Current.UserAppTheme = AppTheme.Light;
+            }
+        }
+
+        private void OnTimerLengthChanged(object sender, ValueChangedEventArgs e)
+        {
+            // Update the timer length label
+            TimerLengthLabel.Text = $"{(int)e.NewValue} seconds";
+        }
     }
 }
+
 
 
